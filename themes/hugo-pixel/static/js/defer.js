1e791042d658a244275c1fb8e17c53a7e614e166
Matt Andreko <mandreko@gmail.com>
2020-10-09 22:42:48 -0400

    Updated jquery and font-awesome

--- themes/hugo-pixel/static/js/defer.js
+++ themes/hugo-pixel/static/js/defer.js
@@ -9,18 +9,18 @@
         document.getElementsByTagName('head')[0].appendChild(stylesheet);
     }
     
-    writeCSS('https://fonts.googleapis.com/css?family=Raleway:400,700%7COpen+Sans:400,700,400italic', 'sha384-jrwaHeZLXvzrq/wEIPsqF8ZfcTPsFqq2PQUJi+grRCsP3s3YDxw1dtGT9IJ77W5s');
-    writeCSS('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.1/css/fontawesome.min.css', 'sha384-vd1e11sR28tEK9YANUtpIOdjGW14pS87bUBuOIoBILVWLFnS+MCX9T6MMf0VdPGq');
-    writeCSS('https:/css/main.css', 'sha384-crvM4YqCVxvzbSflC6A1Ef2DcdxTeCs/9zVxSoJqfpldFQoEmBP90fF3VWbBDOTz');
+    writeCSS('https://fonts.googleapis.com/css?family=Raleway:400,700%7COpen+Sans:400,700,400italic', 'sha384-RSQ9NS1fkHbnW9v603KaA4y7+nE4lqvtUJvzw5FySE5uxR4mFt850VrEILW2ZsHm');
+    writeCSS('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/fontawesome.min.css', 'sha384-vd1e11sR28tEK9YANUtpIOdjGW14pS87bUBuOIoBILVWLFnS+MCX9T6MMf0VdPGq');
+    writeCSS('/css/main.css', 'sha256-TXw+V9w1h2TNy7p6lCpUNTIXHFSrii9swFyGIiSKdkA=');
 
     //if (! $.fn.jquery) {
     if(typeof jQuery!=='undefined'){
-        document.write('<script src="/js/jquery.min.js" integrity="sha384-tsQFqpEReu7ZLhBV2VZlAu7zcOV+rXbYlF2cqB8txI/8aZajjp4Bqd+V6D5IgvKT" crossorigin="anonymous"><\/script>');
+        document.write('<script src="/js/jquery.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"><\/script>');
     }
     //if (! FontAwesome) {
     if(typeof FontAwesome!=='undefined'){
-        document.write('<script src="/js/fontawesome.min.js" integrity="sha384-EMmnH+Njn8umuoSMZ3Ae3bC9hDknHKOWL2e9WJD/cN6XLeAN7tr5ZQ0Hx5HDHtkS" crossorigin="anonymous"><\/script>');
-        document.write('<script src="/js/brands.min.js" integrity="sha384-rUOIFHM3HXni/WG5pzDhA1e2Js5nn4bWudTYujHbbI9ztBIxK54CL4ZNZWwcBQeD" crossorigin="anonymous"><\/script>');
-        document.write('<script src="/js/solid.min.js" integrity="sha384-IA6YnujJIO+z1m4NKyAGvZ9Wmxrd4Px8WFqhFcgRmwLaJaiwijYgApVpo1MV8p77" crossorigin="anonymous"><\/script>');
+        document.write('<script src="/js/fontawesome.min.js" integrity="sha256-TMRxGf3NaZhrxTfD8y/NGiyE4UXBA3udH1l+Co8JDVU=" crossorigin="anonymous"><\/script>');
+        document.write('<script src="/js/brands.min.js" integrity="sha256-lp5km2M1v44bqE38HojZ8uz34u+s+NPPlvmtRM9h8zA=" crossorigin="anonymous"><\/script>');
+        document.write('<script src="/js/solid.min.js" integrity="sha256-AvFW059sTpul/l4lUQdCw21U5Fp9uxldIzvCQrCdf2Q=" crossorigin="anonymous"><\/script>');
         document.write('<link rel="stylesheet" href="/css/fontawesome.min.css" integrity="sha384-vd1e11sR28tEK9YANUtpIOdjGW14pS87bUBuOIoBILVWLFnS+MCX9T6MMf0VdPGq" crossorigin="anonymous">');
     }
